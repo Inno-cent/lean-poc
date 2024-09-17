7415449938cdd89e801ba05c6fadcc3436fe9fb4
inno-cent <ekweozorinnocent@gmail.com>
2024-09-17 15:28:25 +0100

    poc update

--- App.tsx
+++ App.tsx
@@ -7,28 +7,30 @@
 
 import React from 'react';
 import {SafeAreaView, TouchableOpacity, Text, View} from 'react-native';
-import VideoCall from './src/videocall'; 
-import VoiceCall from './src/voicecall'; 
-
+import VideoCall from './src/videocall';
+import VoiceCall from './src/voicecall';
 
 const App = () => {
+  const [activeCall, setActiveCall] = useState(null);
   return (
     <SafeAreaView style={{flex: 1}}>
-     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} onPress={() => setActiveCall('video')}>
-      {!activeCall && (
-        <>
-          <TouchableOpacity >
-            <Text>Start Video Call</Text>
-          </TouchableOpacity>
-          <TouchableOpacity >
-            <Text>Start Voice Call</Text>
-          </TouchableOpacity>
-        </>
-      )}
-      
-      {activeCall === 'video' && <VideoCall />}
-      {activeCall === 'voice' && <VoiceCall />}
-    </View>
+      <View
+        style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+        onPress={() => setActiveCall('video')}>
+        {!activeCall && (
+          <>
+            <TouchableOpacity>
+              <Text>Start Video Call</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text>Start Voice Call</Text>
+            </TouchableOpacity>
+          </>
+        )}
+
+        {activeCall === 'video' && <VideoCall />}
+        {activeCall === 'voice' && <VoiceCall />}
+      </View>
     </SafeAreaView>
   );
 };
