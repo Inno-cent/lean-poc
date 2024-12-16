5a02eb9329f4f506ba847560e62f5cc774d175c9
inno-cent <ekweozorinnocent@gmail.com>
2024-12-16 14:38:46 +0100

    chore : clean up

--- src/screens/callScreen.tsx
+++ src/screens/callScreen.tsx
@@ -51,38 +51,4 @@ const styles = StyleSheet.create({
 });
 
 export default CallScreen;
-// import React from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
 
-// const CallDetailsScreen = ({ route }) => {
-//   const { callDetails } = route.params; // Agora details received from SocketProvider
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.header}>Agora Call Details</Text>
-//       <Text style={styles.text}>Room ID: {callDetails.room_id}</Text>
-//       <Text style={styles.text}>Participant ID: {callDetails.participant_id}</Text>
-//       <Text style={styles.text}>App ID: {callDetails.app_id}</Text>
-//       <Text style={styles.text}>Token: {callDetails.token}</Text>
-//       <Text style={styles.text}>UID: {callDetails.uid}</Text>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   header: {
-//     fontSize: 24,
-//     marginBottom: 20,
-//   },
-//   text: {
-//     fontSize: 18,
-//     marginBottom: 10,
-//   },
-// });
-
-// export default CallDetailsScreen;
