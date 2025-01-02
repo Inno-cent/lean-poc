4f9160a91e04f3b62733a336ea4034a5d39f75c3
inno-cent <ekweozorinnocent@gmail.com>
2025-01-02 12:39:21 +0100

    feat : agora sdk initial setup

--- src/screens/callScreen.tsx
+++ src/screens/callScreen.tsx
@@ -36,7 +36,7 @@ const App = () => {
 
   useEffect(() => {
     setupAgoraEngine();
-
+    joinChannel();
     // Clean up when the component unmounts
     return () => {
       agoraEngineRef.current?.leaveChannel();
